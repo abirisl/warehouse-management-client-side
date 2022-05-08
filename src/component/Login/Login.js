35ebc43ceae22e47654cd2e845b342520dd9f551
abir islam <mhabir979292@gmail.com>
2022-05-08 22:24:41 +0600

    login form make

--- src/component/Login/Login.js
+++ src/component/Login/Login.js
@@ -4,7 +4,22 @@ import { Button, Form } from 'react-bootstrap';
 const Login = () => {
     return (
         <div className='w-25 mx-auto border border-3 mt-5 mb-5 p-5'>
-           
+
+            <h1>Sign In</h1>
+            <hr />
+            <Form>
+                <Form.Group className="mb-3" controlId="formGroupEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formGroupPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Login
+                </Button>
+            </Form>
         </div>
     );
 };
